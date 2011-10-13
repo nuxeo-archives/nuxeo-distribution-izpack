d1b16d82dc32bee25181d9afebd4b3efb4c915f1
Mathieu Guillaume <mg@nuxeo.com>
2011-10-13 09:09:27 +0200

    NXBT-296: Use SID instead of name for Windows group

--- src/main/izpack/setPermissions.bat
+++ src/main/izpack/setPermissions.bat
@@ -3,5 +3,5 @@
 set DIRNAME=.\
 if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
 
-icacls "%DIRNAME%." /grant Users:F /T
+icacls "%DIRNAME%." /grant *S-1-5-32-545:F /T
 rem > %APPDATA%\Nuxeo\log\installer-postinstall.log
